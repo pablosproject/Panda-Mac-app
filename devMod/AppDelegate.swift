967deb71b5cae829ccf0b351d770c25428b9ade0
Paolo Tagliani <pablosproject@gmail.com>
2014-10-23 21:31:24 +0200

    Initial import

---
+++ devMod/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  devMod
+//
+//  Created by Paolo Tagliani on 10/23/14.
+//  Copyright (c) 2014 Paolo Tagliani. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
